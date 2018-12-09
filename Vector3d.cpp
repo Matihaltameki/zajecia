2b48f97bdc9d840f79fc8e33fa6f8321283cb0d1
Matihaltameki <45737902+Matihaltameki@users.noreply.github.com>
2018-12-09 18:47:27 +0100

    add, vector product, scalar product

--- Vector3d.cpp
+++ Vector3d.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
@@ -33,16 +35,33 @@ public:
         return z;
     }
 
+    string print(){
+        stringstream ss;
+        ss << "(" << x << ", " << y << ", " << z << ")";
+        return ss.str();
+    }
+
+    Vector3D add(Vector3D& vector){
+        Vector3D new_vector = Vector3D(x + vector.get_x(), y + vector.get_y(), z + vector.get_z());
+        return new_vector;
+    }
+
+    int scalar_product(Vector3D& vector){
+        return x * vector.get_x() + y * vector.get_y() + z * vector.get_z();
+    }
+
+    Vector3D vector_product(Vector3D& vector){
+        int product_x = y * vector.get_z() - z * vector.get_y();
+        int product_y = - x * vector.get_z() + vector.get_x() * z;
+        int product_z = x * vector.get_y() - vector.get_x() * y;
+
+        Vector3D product_vector = Vector3D(product_x, product_y, product_z);
+        return product_vector;
+    }
+
 private:
     int x;
     int y;
     int z;
 
 };
-
-int main()
-{
-    Vector3D vector = Vector3D(1, 1, 1);
-    cout<< vector.get_x() << endl;
-    return 0;
-}
