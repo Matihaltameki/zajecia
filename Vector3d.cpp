388eab8382dbb1bb6584a1b199bd5fd4ca550693
Matihaltameki <45737902+Matihaltameki@users.noreply.github.com>
2018-12-09 18:16:11 +0100

    constructor and destructor

--- Vector3d.cpp
+++ Vector3d.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 class Vector3D {
 public:
+    Vector3D(int _x, int _y, int _z){
+        x = _x;
+        y = _y;
+        z = _z;
+    }
+
+    ~Vector3D() {}
+
     void set_x(int new_x){
         x = new_x;
     }
@@ -31,3 +39,10 @@ private:
     int z;
 
 };
+
+int main()
+{
+    Vector3D vector = Vector3D(1, 1, 1);
+    cout<< vector.get_x() << endl;
+    return 0;
+}
