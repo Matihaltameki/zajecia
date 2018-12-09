7e93d4078bd6e0b610c4699c2e2deb9005c35be6
Matihaltameki <45737902+Matihaltameki@users.noreply.github.com>
2018-12-09 18:05:12 +0100

    Setters and Getters

---
+++ Vector3d.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+class Vector3D {
+public:
+    void set_x(int new_x){
+        x = new_x;
+    }
+
+    void set_y(int new_y){
+        y = new_y;
+    }
+    void set_z(int new_z){
+        z = new_z;
+    }
+
+    int get_x() {
+        return x;
+    }
+    int get_y() {
+        return y;
+    }
+    int get_z() {
+        return z;
+    }
+
+private:
+    int x;
+    int y;
+    int z;
+
+};
